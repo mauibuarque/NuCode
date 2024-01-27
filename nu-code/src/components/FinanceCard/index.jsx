5cbf8500dd5bd14b53daa8f7fd7021d07f7aa108
Mauí Buarque <maui.buarque1@gmail.com>
2024-01-26 22:21:29 -0300

    feat: FinanceCard

---
+++ nu-code/src/components/FinanceCard/index.jsx
@@ -0,0 +1,17 @@
+const FinanceCard = () => {
+  return (
+    <>
+      <div>
+        <h3>Título</h3>
+        <p>R$ ...</p>
+      </div>
+
+      <div>
+        <p>Tipo</p>
+        <button>Excluir</button>
+      </div>
+    </>
+  );
+};
+
+export default FinanceCard;
