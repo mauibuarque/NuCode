ab84cfd6ecd7968e501f992d09a4a908063ba27f
Mauí Buarque <maui.buarque1@gmail.com>
2024-01-29 20:36:41 -0300

    feat: filterCards

--- nu-code/src/components/FinanceList/index.jsx
+++ nu-code/src/components/FinanceList/index.jsx
@@ -1,9 +1,25 @@
 import style from "./style.module.css";
 
+import { useState } from "react";
+
 import FinanceCard from "../FinanceCard";
 
 const FinanceList = ({ setCardList, formatValue }) => {
   const getCardList = JSON.parse(localStorage.getItem("cardList"));
+
+  const [filterType, setFilterType] = useState("Todos");
+
+  const filterCards = (type) => {
+    setFilterType(type);
+  };
+
+  const filteredCardList = getCardList.filter((card) => {
+    if (filterType === "Todos") {
+      return true;
+    }
+
+    return card.valueType === filterType;
+  });
 
   return (
     <>
@@ -11,15 +27,15 @@ const FinanceList = ({ setCardList, formatValue }) => {
         <div className={style.financeListHeader}>
           <h2>Resumo Financeiro</h2>
           <div className={style.buttons}>
-            <button>Todos</button>
-            <button>Entradas</button>
-            <button>Despesas</button>
+            <button onClick={() => filterCards("Todos")}>Todos</button>
+            <button onClick={() => filterCards("Entrada")}>Entradas</button>
+            <button onClick={() => filterCards("Despesa")}>Despesas</button>
           </div>
         </div>
 
-        {getCardList.length > 0 ? (
+        {filteredCardList.length > 0 ? (
           <ul className={style.cardList}>
-            {getCardList.map((card, index) => (
+            {filteredCardList.map((card, index) => (
               <FinanceCard
                 key={index}
                 card={card}
